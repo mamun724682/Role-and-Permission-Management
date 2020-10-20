dbf254358987decda77ca7cc877719869638ddf2
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-10-20 11:08:01 +0600

    Add seeder data

--- database/seeds/RolePermissionSeeder.php
+++ database/seeds/RolePermissionSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -19,7 +20,42 @@ class RolePermissionSeeder extends Seeder
         $roleUser = Role::create(['name' => 'user']);
 
         // Create Permissions
+        $permissions = [
+            //Dashboard Permissions
+            'dashboard.view',
+
+            //Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            //Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            //Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            //Profile Permissions
+            'profile.view',
+            'profile.edit',
+        ];
 
         // Assign Permission
+        for ($i = 0; $i < count($permissions); $i++) {
+            $permission = Permission::create(['name' => $permissions[$i]]);
+
+            $roleSuperAdmin->givePermissionTo($permission);
+            $permission->assignRole($roleSuperAdmin);
+        }
     }
 }
